ba131aa0763cac7ae04d7fff0d0675f9a0f51a96
Vladimir Kidyaev <krios2146@gmail.com>
2026-03-09 23:27:04 +0700

    feat(hypr): config sync

--- hypr/.config/hypr/scripts/tmux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WINDOW=$(hyprctl clients -j | jq -r '.[] | select(.title == "tmux") | .address' | head -n1)
-
-if [ -n "$WINDOW" ]; then
-    hyprctl dispatch focuswindow "address:$WINDOW"
-else
-    uwsm-app -- xdg-terminal-exec --dir="$(omarchy-cmd-terminal-cwd)" tmux a 2>/dev/null || tmux
-fi
