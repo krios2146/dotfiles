d09ebf351edcf74c02fb6ad0b8fe590fa792de21
Vladimir Kidyaev <krios2146@gmail.com>
2026-05-22 15:50:55 +0400

    feat(scripts): config sync

--- scripts/.config/scripts/webcam-setup.sh
+++ scripts/.config/scripts/webcam-setup.sh
@@ -7,5 +7,5 @@ done
 PHONE_IP=$(ip neighbor show dev enp11s0f3u1 | awk '{print $1}' | head -1)
 
 ffmpeg -i "http://${PHONE_IP}:1488/video" \
-  -vf scale=1280:720,format=yuv420p \
+  -vf scale=720:1280,format=yuv420p \
   -f v4l2 /dev/video0
